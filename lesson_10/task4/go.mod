131a68dd690bcb62c90047eb8e9755d4c6dbd0fb
Aleksey Anikushin <ania@automacon.ru>
2024-04-25 01:00:47 +0300

    123

--- lesson_10/task4/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/AlekseyAnikushin/Go-Developer-Course/lesson_10/task4
-
-go 1.22.1
--- lesson_10/task4/main.go
+++ lesson_10/task4/main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import (
+	v100 "github.com/AlekseyAnikushin/module0"
+	v110 "github.com/AlekseyAnikushin/module1"
+)
 
+func main() {
+	v100.CallHello()
+	v110.CallHello()
 }
